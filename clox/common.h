e715425fc4fa59a53ac9f39a66c0143be237a62f
Brian Gavin <bgavin1996@gmail.com>
2021-12-01 22:01:44 -0800

    clox: 22: local variables

--- clox/common.h
+++ clox/common.h
@@ -8,6 +8,8 @@
 #define DEBUG_PRINT_CODE
 #define DEBUG_TRACE_EXECUTION
 
+#define UINT8_COUNT (UINT8_MAX + 1)
+
 #endif
 // In the book, we show them defined, but for working on them locally,
 // we don't want them to be.
